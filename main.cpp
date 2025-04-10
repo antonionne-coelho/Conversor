493feb4a57787c1bd7195144d8bdd51dcc073c80
antonionne-coelho <antonionnecp7@gmail.com>
2025-04-10 13:46:49 +0000

    versao 2 do conversor

--- main.cpp
+++ main.cpp
@@ -9,20 +9,23 @@ int main(){
     std::cout << "(1) Celsius -> Fahrenheit" << std::endl;
     std::cout << "(2)Fahrenheit -> Celsius" << std::endl;
     std::cout << "Digite sua opção: ";
-    std::cin >> opcao;
-
-    float temp;
-    std::cout << "Digite a temperatura: ";
-    std:: cin >> temp;
-    
-    float conv;
+    std::cin >> opcao;    
 
         switch(opcao){
+            
+            float conv;
+            float temp;  
+            
             case 1:
+     
+                std::cout << "Digite a temperatura: ";
+                std:: cin >> temp;
                 conv = celcius2fahreheit(temp);
                 std::cout << temp <<"Cº = " << conv << "Fº" << std::endl;
                 break; 
             case 2:
+                std::cout << "Digite a temperatura: ";
+                std:: cin >> temp;
                 conv = fahrenheit2celcius(temp);
                 std::cout << temp <<"Fº = " << conv << "Cº" << std::endl;
                 break;
